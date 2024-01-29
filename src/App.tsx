9c9623e6bc5557bb60361947c5f8f894399fd0d6
heion31 <mainit.noah3113@gmail.com>
2024-01-30 00:32:10 +0800

    add: sample content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-5xl font-bold">TEST</h1>;
+  return (
+    <>
+      <h1 className="text-5xl font-bold">TEST</h1>
+      <p className="text-2xl font-medium">by Heionim</p>
+    </>
+  );
 }
-
 export default App;
