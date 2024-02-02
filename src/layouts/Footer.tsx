d0320964aa824d203c8e936128d41e782bf55e5f
heion31 <mainit.noah3113@gmail.com>
2024-02-02 10:12:39 +0800

    fixed: footer year

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="bg-[#062A1F] text-white py-2 text-center absolute bottom-0 w-full">
-      <p className="text-xs">
-        © Copyright 2023 - 2024 | PCC x QCU | All Rights Reserved
+      <p className="text-xs px-4">
+        © Copyright {currentYear} | PCC x QCU | All Rights Reserved
       </p>
     </div>
   );
