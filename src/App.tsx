28c2ee8f5aa357a9f0d808deaf43a9170488369e
heion31 <mainit.noah3113@gmail.com>
2024-02-02 10:04:34 +0800

    add: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Footer from "./layouts/Footer";
 import Header from "./layouts/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="bg-[#062A1F] text-white py-2 text-center absolute bottom-0 w-full">
+      <p className="text-xs">
+        © Copyright 2023 - 2024 | PCC x QCU | All Rights Reserved
+      </p>
+    </div>
+  );
+}
