6b9ec46434d2972f7d1b54ba6c534279d373cbaa
heion31 <mainit.noah3113@gmail.com>
2024-02-02 13:18:28 +0800

    create NavLinks component

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,10 +1,12 @@
 import logo from "../assets/pcc-logo.svg";
+import NavLinks from "../components/NavLinks";
 
 export default function Header() {
   return (
     <header className="bg-[#204C3E] text-[#FCFCFC] px-4 sm:px-8 md:px-16 lg:px-32 py-4 border-b-4 border-b-[#D9D9D9] fixed top-0 z-50 w-full">
       <div className="container mx-auto">
         <div className="flex flex-col sm:flex-row items-center justify-between">
+          {/* Logo */}
           <div className="flex items-center mb-2 sm:mb-0">
             <a href="/">
               <img src={logo} alt="Logo" className="h-12 w-auto mr-2" />
@@ -21,40 +23,9 @@ export default function Header() {
               </p>
             </div>
           </div>
-          <ul className="hidden sm:flex flex-col sm:flex-row gap-x-4 sm:gap-x-4 md:gap-x-7 lg:gap-x-14 text-[1rem]">
-            <li>
-              <a
-                href="/"
-                className="border-b-2 border-transparent hover:border-white py-2"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="/cancer-data"
-                className="border-b-2 border-transparent hover:border-white py-2"
-              >
-                Cancer Data
-              </a>
-            </li>
-            <li>
-              <a
-                href="/data-methods"
-                className="border-b-2 border-transparent hover:border-white py-2"
-              >
-                Data & Methods
-              </a>
-            </li>
-            <li>
-              <a
-                href="/affiliates"
-                className="border-b-2 border-transparent hover:border-white py-2"
-              >
-                Affiliates
-              </a>
-            </li>
-          </ul>
+
+          {/* Navigation Links */}
+          <NavLinks />
         </div>
       </div>
     </header>
